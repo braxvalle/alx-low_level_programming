5d99d2a69363a33d7fe50c16436344f28bfa0cd7
braxvalle <braxvalle2@gmail.com>
2022-03-12 07:58:22 -0800

    sice.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 
 	printf("Size of a char: %lu byte\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte\n", (unsigned long)sizeof(j));
-	printf("Size of an long: %lu byte\n", (unsigned long)sizeof(k));
+	printf("Size of an long int: %lu byte\n", (unsigned long)sizeof(k));
 	printf("Size of long long int: %lu byte\n", (unsigned long)sizeof(l));
 	printf("Size of float: %lu byte\n", (unsigned long)sizeof(f));
 	return (0);
