b88231af735a03b46e7cbe83352b151232cba6db
root <braxvalle@gmail.com>
2022-03-12 06:19:43 -0800

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - entry point of the program
+ *
+ * return 0 - determines success
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
