5179262c9decfd87606d6fdebe4c372bb001f6cf
braxvalle <braxvalle2@gmail.com>
2022-03-15 21:24:05 +0300

    functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,22 @@
+#include <main.h>
 #include <stdio.h>
 
-int main(){
-	int w;
+/**
+ *Main - Entry point
+ *Return:always 0 (success)                                    
+ */
 
-	w = getchar();
-	while (c != EOF) {
-		putchar(w);
-		w = getchar(w);
+
+int main(void){
+
+	int n = 0;
+	char msg[] = "_putchar\n";
+	char c;
+	while (n <= 8)
+	{
+		c = msg[n];
+		_putchar(c);
+		n++;
 	}
+	return (0);
 }
