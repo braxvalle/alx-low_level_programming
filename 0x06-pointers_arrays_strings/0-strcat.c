8d04cac04b39449e1343c750fccf7f708e7efb90
braxvalle <braxvalle2@gmail.com>
2022-03-23 21:01:34 +0300

    _strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,17 +9,17 @@
 char *_strcat(char *dest, char *src)
 {
 	int i;
-	int j;
+	int n;
 
 	i = 0;
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
-	j = 0;
-	while (src[j] != '\0')
+	n = 0;
+	while (src[n] != '\0')
 	{
-		dest[i] = src[j];
+		dest[i] = src[n];
 		i++;
 		j++;
 	}
