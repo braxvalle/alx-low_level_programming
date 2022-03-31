f48816ccb63ca349193ff8cbd5692ea391017ce7
braxvalle <braxvalle2@gmail.com>
2022-03-30 18:33:30 -0700

    mem cp

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
 /**
- * memset - Fills the first n bytes of the memory area
- *          pointed to by @s with the constant byte @c.
- * @s: A pointer to the memory area to be filled.
- * @c: The character to fill the memory area with.
- * @n: The number of bytes to be filled.
+ *_memcpy - a function that copies memory area
+ *@dest: memory where is stored
+ *@src: memory where is copied
+ *@n: number of bytes
  *
- * Return: A pointer to the filled memory area @s.
+ *Return: copied memory with n byted changed
  */
-void *_memset(void *s, int c, size_t n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
+	int i = 0;
+	int j = 0;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-
-	return (memory);
+	while (n > 0)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+		n--;
+	}
+	return (dest);
 }
-
