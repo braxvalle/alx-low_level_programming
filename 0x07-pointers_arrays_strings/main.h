cdad57a77faaf3c034d52d24f721a026c6250cc2
braxvalle <braxvalle2@gmail.com>
2022-03-30 19:03:44 -0700

    main

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,11 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 #include <stddef.h>
 
-int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
-char *_memcpy(char *dest, char *src, unsigned int n);
+void _putchar (char c);
+void *_memset (char *s, char b, unsigned int n);
+void *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
